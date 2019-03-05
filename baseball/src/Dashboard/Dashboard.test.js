c25b9a126435ad529fea788c32176a2f976cda33
Kingaa6 <Kingaa6@gmail.com>
2019-03-05 12:21:44 -0800

    Add test for dashboard buttons

--- baseball/src/Dashboard/Dashboard.test.js
+++ baseball/src/Dashboard/Dashboard.test.js
@@ -8,4 +8,18 @@ describe('<Dashboard />', () => {
   it('renders without crashing', () => {
     render(<Dashboard />);
   });
+
+  it('renders buttons', () => {
+    const { getByText } = render(<Dashboard />);
+
+    const strike = getByText(/add strike/i);
+    const ball = getByText(/add ball/i);
+    const foul = getByText(/add foul/i);
+    const hit = getByText(/player hits/i);
+
+    expect(strike).toBeInTheDocument();
+    expect(ball).toBeInTheDocument();
+    expect(foul).toBeInTheDocument();
+    expect(hit).toBeInTheDocument();
+  });
 });
